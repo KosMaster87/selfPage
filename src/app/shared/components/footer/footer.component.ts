147117eb49378c43ddef33146890a8796db3e7d6
Konstantin Aksenov <kav.pbg@gmail.com>
2024-12-02 16:58:03 -0300

    inprint start

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { SharedModule } from './../../../future-modul/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, SharedModule],
+  imports: [RouterLink, SharedModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrls: [
     './footer.component.scss',
